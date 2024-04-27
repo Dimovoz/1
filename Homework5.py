38c30bca3b5811377e5744881bae4d60f3f2b225
Dimovoz <166947006+Dimovoz@users.noreply.github.com>
2024-04-27 22:15:21 +0300

    Add files via upload

---
+++ Homework5.py
@@ -0,0 +1,13 @@
+my_list = ['lime','lemon','nut','apple','grape', 'corn']
+print(my_list),
+print(my_list[0])
+print(my_list[-1])
+print(my_list[2:5])
+my_list[2] = 'coconut'
+print(my_list)
+
+my_dict = {'name' : 'Dmitry' , 'surname' : 'Pavlovsky' , 'age' : 47}
+print(my_dict)
+print(my_dict['name'])
+my_dict['weit'] = 90
+print(my_dict)
