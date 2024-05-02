3d6bd90a02935e98005125119f9b343c451b4d83
Dimovoz <166947006+Dimovoz@users.noreply.github.com>
2024-05-02 20:51:40 +0300

    Update Homeworks6.py

--- Homeworks6.py
+++ Homeworks6.py
@@ -2,12 +2,6 @@ list_ = ['BMW', "MB", "Lada", 'KIA', "Honda"]
 string = 'Я езжу на автомабиле марки '
 cars_count = 0
 
-for i in range(1):
-    print(string, list_[0])
-    print(string, list_[1])
-    print(string, list_[2])
-    print(string, list_[3])
-    print(string, list_[4])
-    for i in list_:
-        cars_count += 10
-        print(cars_count)
+for i in range(len(list_)):
+    cars_count += 10
+    print(string, list_[i], cars_count)
