28ca25dafe57237a2f465e3562773f9eee164daa
Dimovoz <166947006+Dimovoz@users.noreply.github.com>
2024-05-06 00:15:52 +0300

    Update 11.py
    доработка

--- 11.py
+++ 11.py
@@ -4,12 +4,7 @@ students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
 list_ = list(students)
 list_.sort()
 
-sA = list_[0]
-sB = list_[1]
-sJ = list_[2]
-sK = list_[3]
-sS = list_[4]
-
+sA, sB, sJ, sK, sS = list_[0: 5]
 nA = sum(grades[0]) / len(grades[0])
 nB = sum(grades[1]) / len(grades[1])
 nJ = sum(grades[2]) / len(grades[2])
