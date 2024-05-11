9f2287440289713310442deb176a39964b59f693
Dimovoz <166947006+Dimovoz@users.noreply.github.com>
2024-05-11 09:20:03 +0300

    Add files via upload

---
+++ homework10.py
@@ -0,0 +1,23 @@
+def test(*arg):
+    print(arg)
+
+test("DD", 23, True)
+
+
+n = int(input(f'Расчёт факториала: '))
+def fact(n):
+    a = 1
+    for i in range(1, n + 1):
+        a *= i
+    return a
+
+print(fact(n))
+
+def factor(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factor(n - 1)
+
+print(factor(n))
+
