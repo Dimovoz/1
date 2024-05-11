f8e60f38cc4034cd7e5b6b8d61102fccdc72e5c4
Dimovoz <166947006+Dimovoz@users.noreply.github.com>
2024-05-11 09:41:18 +0300

    Update homework10.py
    Доработал

--- homework10.py
+++ homework10.py
@@ -5,19 +5,23 @@ test("DD", 23, True)
 
 
 n = int(input(f'Расчёт факториала: '))
-def fact(n):
-    a = 1
-    for i in range(1, n + 1):
-        a *= i
-    return a
+while n < 0:
+    print(f'Число должно быть положительным')
+    break
+else:
 
-print(fact(n))
+    def fact(n):
+        a = 1
+        for i in range(1, n + 1):
+            a *= i
+        return a
 
-def factor(n):
-    if n == 0:
-        return 1
-    else:
-        return n * factor(n - 1)
+    print(fact(n))
 
-print(factor(n))
+    def factor(n):
+        if n == 0:
+            return 1
+        else:
+            return n * factor(n - 1)
 
+    print(factor(n))
